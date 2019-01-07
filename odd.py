308340b95a9093b9bc05ba7294e80e4e22bc74b0
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-07 16:03:49 +0530

    Update and rename odd.py to vowel.py

--- odd.py
+++
@@ -1,8 +0,0 @@
-n=int((input()))
-if n%2==0 and n>0:
-	print("Even")
-elif n%2==1 and n>0:
-	print("Odd")
-else:
-    print("invalid")
-	
---
+++ vowel.py
@@ -0,0 +1,11 @@
+c=input()
+a=[a,e,i,o,u]
+b=[b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z]
+for i in range(0,len(a)):
+	if c==a[i]:
+		print("Vowel")
+for j in range(0,len(b)):
+    if c==b[j]:
+    	print("Consonant")
+print("invalid")
+		
