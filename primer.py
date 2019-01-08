af91251d1a99d6a39f743dfa4dd1ff1b20335fc5
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 18:34:37 +0530

    Create primer.py

---
+++ primer.py
@@ -0,0 +1,13 @@
+m,n=map(int,input().split())
+x=' '
+flag=0
+for n in range(m+1,n):
+	if n>0:
+		for i in range(2,n):
+			if n%i==0:
+				break
+		else:
+			x=x+str(n)+' '
+print(x.strip())			
+	
+		
