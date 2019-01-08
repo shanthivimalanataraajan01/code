5e783467c42e529314095057c9c3c2544b3fbeec
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 15:38:40 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,5 +1,14 @@
-y=int(input())
-if y%4==0:
+x=int(input())
+if x%4==0:
 	print("yes")
 else:
-	print("no")
+	print("no")y=int(input())
+2
+if y%4==0:
+3
+        print("yes")
+4
+else:
+5
+        print("no")
+6
