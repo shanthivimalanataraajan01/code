7bfb9f645d8486ac1fcd44d76bbdf36a9c7a03da
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 16:19:07 +0530

    Update sumn.py

--- sumn.py
+++ sumn.py
@@ -4,10 +4,4 @@ if n>=k:
 	s=[]
 	for i in range(0,k):
 		s.append(x[i])
-	print(sum(s))	n,k=map(int,input().split())
-x=[int(i) for i in input().split()]
-if n>=k:
-	s=[]
-	for i in range(0,k):
-		s.append(x[i])
 	print(sum(s))	
