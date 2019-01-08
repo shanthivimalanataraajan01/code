0b19902ae42f78115e4071df0412ac1f042cb08e
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 16:48:02 +0530

    Create vowel1.py

---
+++ vowel1.py
@@ -0,0 +1,7 @@
+s=int(input())
+if s=='a'  or s=='e' or s=='i' or s=='o' or s=='u':
+	print("Vowel")
+elif s!='a'or s!='e' or s!='i' or s!='o' or s!='u':
+	print("Consonant")
+else:
+	print("invalid")
