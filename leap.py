97bc1330344872110e44c03fc183347ba6abf18c
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 15:36:55 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -2,8 +2,4 @@ y=int(input())
 if y%4==0:
 	print("yes")
 else:
-	print("no")y=int(input())
-if y%4==0:
-	print("yes")
-else:
-	print("no")
+	print("no")https://github.com/shanthivimalanataraajan01/code/edit/master/leap.py
