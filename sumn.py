91c35ebd3cd4d0cb95bc24118696a76cdc266a05
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-08 16:17:23 +0530

    Create sumn.py

---
+++ sumn.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+x=[int(i) for i in input().split()]
+if n>=k:
+	s=[]
+	for i in range(0,k):
+		s.append(x[i])
+	print(sum(s))	n,k=map(int,input().split())
+x=[int(i) for i in input().split()]
+if n>=k:
+	s=[]
+	for i in range(0,k):
+		s.append(x[i])
+	print(sum(s))	
