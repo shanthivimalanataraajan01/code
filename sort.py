83a5bd3b9537189e6287747fcbb43031f5be0299
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-24 15:55:14 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,4 +2,8 @@ w=int(input())
 e=[int(i) for i in input().split()]
 s=sorted(e)
 for i in range(0,len(e)):
-	print(s[i],end=" ")
+	if(i == (len(e)-1)):
+		print(s[i],end="")
+	else:
+		print(s[i],end=" ")
+
