b4fe45263c3e0a4ce1c0d92c44152020bcc43bd7
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-29 16:53:13 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,12 +1,5 @@
-#vim
-#44
-n=int(input())
-l=[1,2,3,4,5,6,7,8,9,10]
-for i in range(len(l)):
-	if n in l:
-		print("yes")
-		break
-	else:
-		print("no")
-		break
-	
+a=int(input())
+if a>=1 and a<=10:
+	print("yes")
+else:
+	print("no")
