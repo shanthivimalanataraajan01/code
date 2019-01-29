fc420e9db49fd7f79ea030fed896a968c1aa751c
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-01-29 16:48:16 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,11 +1,5 @@
-#44
-n=int(input())
-l=[1,2,3,4,5,6,7,8,9,10]
-for i in range(len(l)):
-	if n in l:
-		print("yes")
-		break
-	else:
-		print("no")
-		break
-	
+a,b=map(str,input().split())
+if len(a)>=len(b):
+	print(a)
+else:
+	print(b)
