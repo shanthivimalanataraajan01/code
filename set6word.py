f19b756f48730180fb32be92eddd59f8c7e68186
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-02-13 15:39:34 +0530

    Create set6word.py

---
+++ set6word.py
@@ -0,0 +1,22 @@
+a=int(input())
+if a%2==1:
+	print("one")
+if a%2==2:
+	print("two")
+if a%2==3:
+	print("three")
+if a%2==4:
+	print("four")
+if a%2==5:
+	print("five")
+if a%2==6:
+	print("six")
+if a%2==7:
+	print("seven")
+if a%2==8:
+	print("eight")
+if a%2==9:
+	print("nine")
+if a%2==10:
+	print("ten")
+										
