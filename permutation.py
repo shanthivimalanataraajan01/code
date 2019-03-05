1da691aef35b4bc214b7ac574f639463ce6e97dc
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-03-05 12:51:28 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,21 +1,5 @@
-# your code goes here
-def permutation(li):
-	if l(li)==0:
-		return []
-	if l(li)==1:
-		return[li]
-	k=[]
-	for i in range(l(li)):
-		a=li[i]
-		rem=li[:i]+li[i+1:]
-		for m in permutation(rem):
-			b.append([a]+m)
-	return b
-		
-n=list(input())
-for i in permutation(n):
-	for c in i:
-		print(c,end="")
-	print()
-
-      
+from itertools import permutations
+a=input()
+p=permutations(a)
+for i in list(p):
+	print(''.join(i))
