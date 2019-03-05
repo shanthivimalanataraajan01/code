7b25960d53419d232340ed106ac545265c1f6d69
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-03-05 10:27:05 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,20 +1,20 @@
-s,r=map(str,input().split())
-l=[]
+f,t=map(str,input().split())
+p=[]
 k=[]
 c=0
-if len(s)==len(r):
-    for i in s:
-        a=s.count(i)
-        l.append(a)
-    for j in r:
-        b=r.count(j)
+if len(f)==len(t):
+    for i in f:
+        a=f.count(i)
+        p.append(a)
+    for j in t:
+        b=t.count(j)
         k.append(b)
-    for m in range(0,len(l)):
+    for m in range(0,len(p)):
         for n in range(0,len(k)):
             if m==n:
-                if l[m]==k[n]:
-                    c+=1
-    if c==len(l):
+                if p[m]==k[n]:
+                    c=c+1
+    if c==len(p):
         print("yes")
     else:
         print("no")
