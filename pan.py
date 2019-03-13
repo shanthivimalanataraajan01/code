1446dd5b2df5c12d46d697a20368a1d6191eb08a
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-03-14 00:13:02 +0530

    Create pan.py

---
+++ pan.py
@@ -0,0 +1,10 @@
+# your code goes heremsd
+import string,sys
+def ispangram(s,alphabet=string.ascii_lowercase):
+	a=set(alphabet)
+	if a<=set(s.lower()):
+		return ("yes")
+	else:
+		return ("no")
+s=input()
+print(ispangram(s))
