44c5432cfaa124708520794a214a236282073f3c
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-03-13 23:12:02 +0530

    Update alphanumericbeg.py

--- alphanumericbeg.py
+++ alphanumericbeg.py
@@ -1,7 +1,11 @@
 #ms
-string = input()
-a=0
-for i in string:
-	if(i.isnumeric()):
-		a = a + 1
-	print(a,end='')
+n=input()
+c=[]
+for i in n:
+	if i.isdigit():
+		c.append(i)
+for j in range(0,len(c)):
+	if j==len(c)-1:
+		print(c[j])
+	else:
+		print(c[j],end="")
