7ced1ff43d5432b20039bd88fef608a63ea0bb8c
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-05-06 09:04:20 +0530

    Update huntunique.py

--- huntunique.py
+++ huntunique.py
@@ -1,18 +1,17 @@
-# your code goes here
-n=int(input())
-l=list(map(int,input().split()))
-l1=[]
-c=0
-a=set([i for i in l if l.count(i)==1 ])
-for i in a:
-	l1.append(i)
-	c=1
-l1.sort
-if c==1:
-	for i in range(len(l1)):
-		if i==len(l1)-1:
-			print(l1[i],end='')
-		else:
-			print(l1[i],end=' ')
+n = input()
+a = map(int,raw_input().split())
+a.sort()
+s = [55]
+for i in range(0,n-1):
+    if a[i] == a[i+1]:
+        p = s.pop()
+        if a[i] == p:
+          s.append(p)
+        else:
+          s.append(p)
+          s.append(arr[i])
+s.pop(0)
+if s == []:
+    print 'unique'
 else:
-	print('unique')
+    print (' '.join(map(str,s)))
