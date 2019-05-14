745e5c53a28cc2d4b88e84bf27c2552735361ccc
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-05-14 16:18:36 +0530

    Update 44pro.py

--- 44pro.py
+++ 44pro.py
@@ -1,11 +1,5 @@
 n,p,q,r=map(int,input().split())
-a=list(map(int,input().split()))
+a=[int(i) for i in input().split()]
 b=[]
-for i in range(0,len(a)):
-	for j in range(0,len(a)):
-		for k in range(0,len(a)):
-			if i<=j<=k:
-				c=p*a[i]+q*a[j]+r*a[k]
-				b.append(c)
-print(max(b))
-© 2019 GitHub, Inc.
+c=[p*a[i]+q*a[j]+r*a[k] for i in range(len(a)) for j in range(len(a)) for k in range(len(a)) if i<=j<=k]
+print(max(c))
