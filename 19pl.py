a9fea3e7c55d29d127be1d2ab3a8c4fe9da83a81
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-05-14 10:00:34 +0530

    Update 19pl.py

--- 19pl.py
+++ 19pl.py
@@ -1,14 +1,20 @@
-#ms
-import sys, string, math
-a = int(input())
-b = a
-l = []
-for i in range( 2,b) :
-    if a%i == 0: 
-      l.append(i)
-    while a%i == 0 : 
-      a //= i
-if len(l) == 0 : 
-  print(b)
-else :          
-   print(*l)
+#rev
+def isPrime(n):
+    c=1
+    for i in range(2,n):
+        if(n%i==0):
+            c=0
+            break
+    return c
+x=int(input())
+s=""
+l=[]
+for i in range(2,x+1):
+    if(x%i==0):
+        r=isPrime(i)
+        if(r==1):
+            l.append(i)
+for i in sorted(l):
+    s=s+str(i)+" "
+print(s.strip())
+        
