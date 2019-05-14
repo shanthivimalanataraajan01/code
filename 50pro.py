0bae128be7efddf818969c481f94937428137d2d
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-05-14 16:26:44 +0530

    Update 50pro.py

--- 50pro.py
+++ 50pro.py
@@ -1,21 +1,21 @@
-a,b=map(int,input().split())
-c=[]
-for i in range(a):
-  c.append(list(map(int,input().split())))
+n,m=map(int,input().split())
+e=[]
+for i in range(m):
+  e.append(list(map(int,input().split())))
 cost=10000000
-e=0
-for i in range(a):
-  if c[i][0]==1:
-    s=c[i][1]
-    d=c[i][2]
-    for j in range(i+1,a):
-      if c[j][0]==s:
-        s=c[j][1]
-        d+=c[j][2]
-    if d<cost and s==b:
-      cost=d
-      e+=1
-if e==0:
+f=0
+for i in range(m):
+  if e[i][0]==1:
+    s=e[i][1]
+    c=e[i][2]
+    for j in range(i+1,m):
+      if e[j][0]==s:
+        s=e[j][1]
+        c+=e[j][2]
+    if c<cost and s==n:
+      cost=c
+      f+=1
+if f==0:
   print(-1)
 else:
   print(cost)
