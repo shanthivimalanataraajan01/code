10c6f398079f9c86fec01d6e375f1cf0f6edec42
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-05-14 08:58:35 +0530

    Update 7pl.py

--- 7pl.py
+++ 7pl.py
@@ -1,15 +1,3 @@
-# your code goes here
-a=str(input())
-b=[]
-l=len(a)
-i=0
-while(i<l-1):
-    b.append(a[i+1])
-    b.append(a[i])
-    i=i+2
-if(l%2==0):
-    print("".join(b))
-else:
-    a.append(a[l-1])
-    print("".join(b))
-    
+#ms
+n= input()
+print(''.join([ s[x:x+2][::-1] for x in range(0, len(s), 2) ]))
